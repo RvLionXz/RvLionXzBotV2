a24f078ef93ba40b5266870024e4d11bb2c92165
RvLionXz <rvlionxz@gmail.com>
2023-10-15 23:20:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // APIKEY OPEN AI
-global.jeropenai  = "ISI APIKEY LU AMBIL DI OPENAI.COM" // ISI APIKEY LU
+global.jeropenai  = "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" // ISI APIKEY LU
 
 // Api
 global.APIs = {
@@ -14,26 +14,26 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'V19', // ISI APIKEY LU
 }
 
-global.sewabot = ("SEWA CHAT OWNER") // ISI HARGA SEWA BOT LU
+global.sewabot = ("5k Aja") // ISI HARGA SEWA BOT LU
 
-global.namabot = "JER-BOTZ MD" // UBAH JADI NAMA LU
-global.namaowner = "JER" // NAMA OWNER
-global.footer_text = "© JER-BOTZ MD" + namabot // NAMA BOT
+global.namabot = "RvLionXz" // UBAH JADI NAMA LU
+global.namaowner = "Reval" // NAMA OWNER
+global.footer_text = "RvLionXz" + namabot // NAMA BOT
 global.pp_bot = fs.readFileSync("./image/allmenubot.jpg") // FOTO BOT MAX 50KB BIAR GA DELAY
 global.qris = fs.readFileSync("./image/qris.jpg") // FOTO QRIS MAX 50KB BIAR GA DELLAY
 global.owner = ['6283136394680'] // UBAH NOMOR YANG MAU DI JADIKAN OWNER
-global.packname = '© LISA-BOTZ' //sticker wm ubah
-global.author = 'Di Buat Oleh LISA' //sticker wm ganti nama kalian
+global.packname = 'RvLionXz Bot' //sticker wm ubah
+global.author = 'Di Buat Oleh RvLionXz' //sticker wm ganti nama kalian
 // = \\
 global.sessionName = 'session' // GAK USAH UBAH
 global.prefa = ['', '!', '.', '🐦', '🐤', '🗿'] // GAK USAH UBAH
-global.fakelink = "https://s.id/jerajaa" // bebas asal jan hapus
-global.grubbot = (`*INI KAK LINK GRUB NYA*\n\nhttps://chat.whatsapp.com/KTXtrESxZCg8aTUbP62c6d`) // GANTI LINK GRUB BOT LU \\
+global.fakelink = "https://github.com/RvLionXz" // bebas asal jan hapus
+global.grubbot = (`*LINK GITHUB*\n\nhttps://github.com/RvLionXz`) // GANTI LINK GRUB BOT LU \\
 // - \\
 // FALSE OR TRUE \\
-global.autoTyping = false // BEBAS
-global.welcome = false // KALO MAU AUTO WELCOME UBAH JADI true
-global.left = false // KALO MAU AUTO LEFT UBAH JADI true
+global.autoTyping = true // BEBAS
+global.welcome = true // KALO MAU AUTO WELCOME UBAH JADI true
+global.left = true // KALO MAU AUTO LEFT UBAH JADI true
 global.anticall = true // BEBAS
 
 let file = require.resolve(__filename)
